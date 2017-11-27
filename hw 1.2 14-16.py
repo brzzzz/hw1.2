067f3b3beabbcc723c4211d667bbcdc406c909e1
brzzzz <dimabereza007@gmail.com>
2017-11-27 20:56:39 +0200

    init commit

--- hw 1.2 14-16.py
+++ hw 1.2 14-16.py
@@ -28,6 +28,35 @@ else:
 # Каждая окружность задается координатами центра и радиусом.
 
 
+x1 = 7
+x2 = 5
+y1 = 5
+y2 = 7
+r1 = 2
+r2 = 2
+
+center_to_center_distance = (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+radius_sum = r1 + r2
+
+
+def is_intersect(center_to_center_distance, radius_sum):
+    if center_to_center_distance <= radius_sum:
+        print('Не пересекаются')
+        return True
+    else:
+        print('Пересекаются')
+        return False
+
+
+is_intersect(center_to_center_distance, radius_sum)
+
+
+
+
+
+
+
 
 
 
